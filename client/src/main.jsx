db29e59f1663eb64fcbd6fda706516c367945b15
DavidRengifo12 <davidsantiagorengifoguacheta@gmail.com>
2025-03-29 17:14:47 -0500

    entrega maqueta feedback

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,12 @@ import HeaderHome from "./pages/Home/HeaderHome"
 
 import "../src/pages/Home/styles/indexHome.css"
 import "../src/pages/Home/styles/HeaderHome.css"
+import Combinar from './pages/CombinarFA/Combinar'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HeaderHome />
+    <Combinar />
   </StrictMode>,
 )
