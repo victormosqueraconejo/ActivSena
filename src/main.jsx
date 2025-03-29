09ce323fe8097d1af1f91c50428babd63a9757f0
Victor <victormanuelmosqueraconejo@gmail.com>
2025-03-29 12:38:38 -0500

    Cambios de main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Routes, Route } from "react-router-dom";
 import MenuLateral from "../client/src/pages/DashBoard/DashBoardComponents/MenuLateral";
-import Navbar from "../client/src/pages/pages/DashBoard/DashBoardComponents/Navbar";
-import ActivBot from "../client/src/pages/pages/DashBoard/DashBoardComponents/ActivBot";
-import HomeDash from "../client/src/pages/pages/DashBoard/HomeDash";
+import Navbar from "../client/src/pages/DashBoard/DashBoardComponents/Navbar";
+import ActivBot from "../client/src/pages/DashBoard/DashBoardComponents/ActivBot";
+import HomeDash from "../client/src/pages/DashBoard/HomeDash";
 
 import "../client/src/styles/global.css";
 import "../client/src/styles/ColaViento.css";
 import "../client/src/styles/BotHp.css";
-import "..client/src/styles/Resposive.css";
+import "../client/src/styles/Resposive.css";
 import "../client/src/styles/Secciones.css";
+import "../client/src/styles/App.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
     <section className="contenedor">
       <MenuLateral />
       <main className="contenido">
@@ -26,6 +26,5 @@ createRoot(document.getElementById('root')).render(
       </main>
       <ActivBot />
     </section>
-
   </StrictMode>,
 )
