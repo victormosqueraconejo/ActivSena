b17fa62c30037976f04bc7a91a06a8c504a19f9d
Victor <victormanuelmosqueraconejo@gmail.com>
2025-03-29 12:24:10 -0500

    Cambios de main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Routes, Route } from "react-router-dom";
+import MenuLateral from "../client/src/pages/DashBoard/DashBoardComponents/MenuLateral";
+import Navbar from "../client/src/pages/pages/DashBoard/DashBoardComponents/Navbar";
+import ActivBot from "../client/src/pages/pages/DashBoard/DashBoardComponents/ActivBot";
+import HomeDash from "../client/src/pages/pages/DashBoard/HomeDash";
 
+import "../client/src/styles/global.css";
+import "../client/src/styles/ColaViento.css";
+import "../client/src/styles/BotHp.css";
+import "..client/src/styles/Resposive.css";
+import "../client/src/styles/Secciones.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+
+    <section className="contenedor">
+      <MenuLateral />
+      <main className="contenido">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomeDash />}></Route>
+        </Routes>
+      </main>
+      <ActivBot />
+    </section>
+
   </StrictMode>,
 )
