87bb7d33ae031ffab1f40eca7257979047e5e325
DavidRengifo12 <davidsantiagorengifoguacheta@gmail.com>
2025-03-30 10:22:53 -0500

    maqueta de chat Maikol Daza

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,7 @@ import { createRoot } from 'react-dom/client'
 import Combinar from './pages/CombinarFA/Combinar'
 import RutasComponents from './RutasComponents.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RutasComponents />
