150e746e71caaa59bd91740eba89ceb23bf59abd
ALexjh117 <alexchguendo01@gmail.com>
2025-03-29 19:13:23 -0500

    maquetas

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,14 @@ import { createRoot } from 'react-dom/client'
 
 
 import RutasComponents from './RutasComponents.jsx'
-
-
+import Alquiler from './pages/Alquiler/Alquiler.jsx'
+import DetallesAlquiler from './pages/DetallesAlquier/DetallesAlquiler.jsx'
+import Actividades from './pages/Actividades/Actividades.jsx'
+import RegistroLudicas from './pages/RegistroLudicas/Registroludicas.jsx'
+import Constancia from './pages/ConstanciaCertificado/Constancia.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
-      <RutasComponents ></RutasComponents>
+  <Constancia />
   
   </StrictMode>,
 )
