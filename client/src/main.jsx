74352221a4ad77f885534a3a0c2273c2b6f6f3e1
ALexjh117 <alexchguendo01@gmail.com>
2025-03-30 08:47:54 -0500

    Horas ludicas

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,13 @@ import { createRoot } from 'react-dom/client'
 
 
 import RutasComponents from './RutasComponents.jsx'
+import DetallesAlquiler from './pages/DetallesAlquiler/DetallerAlquiler.jsx'
+import Actividades from './pages/Actividades/Actividades.jsx'
+import HorasLudicas from './pages/HorasLudicas/HorasLudicas.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <RutasComponents />
+
   
   </StrictMode>,
 )
